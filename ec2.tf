5f4b5406aa30b6f22cd6d2567e7355bcc9369a49
KameshvaranV <103570790+KameshvaranV@users.noreply.github.com>
2022-07-26 14:09:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "MyTerraformec2"{
     ami = "ami-0cff7528ff583bf9a"
     instance_type = "t2.micro"
-    security_groups = "sg-046dc129e40c52823"
+    secgroupname = "mysecgrp"
     key_name = "may31keypair"
     tags = {
         Name = "MyTerraformec2"
