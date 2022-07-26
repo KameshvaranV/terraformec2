487e5b2eeff37209fdd9611e095531290e5219e1
KameshvaranV <103570790+KameshvaranV@users.noreply.github.com>
2022-07-26 13:01:05 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "MyTerraformec2"{
+    ami = "ami-0cff7528ff583bf9a"
+    instance_type = "t2.micro"
+    security_group = "sg-046dc129e40c52823"
+    keypair_name = "may31keypair"
+    tags = {
+        Name = "MyTerraformec2"
+    }
+}
